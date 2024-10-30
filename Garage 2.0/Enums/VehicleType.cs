e7422f80c4d8e249033aa6e031e563167b0e9d41
dansaa8 <dansaa8@protonmail.com>
2024-10-30 11:00:07 +0100

    Create model for Vehicle

---
+++ Garage 2.0/Enums/VehicleType.cs
@@ -0,0 +1,13 @@
+﻿namespace Garage_2._0
+{
+    public enum VehicleType
+    {
+        Car = 0,
+        Vehicle = 1,
+        Bus = 2,
+        Motorcycle = 3,
+        AirPlane = 4,
+        Boat = 5,
+        HoverCraft = 6,
+    }
+}
---
+++ Garage 2.0/Models/Entities/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Garage_2._0.Models.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public uint Wheels { get; set; }
+
+        public DateTime ArriveTime { get; set; }
+
+        [StringLength(20)]
+        public string Color { get; set; }
+
+        [StringLength(20)]
+        public string RegNr { get; set; }
+
+        [StringLength(20)]
+        public string Model {  get; set; }
+
+        [StringLength(20)]
+        public string Brand { get; set; }
+
+        public VehicleType VehicleType { get; set; }
+        
+    }
+}
