383cbed2b62bec341f5abd4e943bccd82062602d
SinonSoderlund <sinonlynx@gmail.com>
2024-11-18 16:10:52 +0100

    added-indentity to project, not fully functional but wokrable as base to work from

---
+++ Garage 2.0/Models/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Garage_2._0.Models.Entities
+{
+    public class User : IdentityUser
+    {
+        public int PersonalNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
--- Garage 2.0/Program.cs
+++ Garage 2.0/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Garage_2._0.Data;
 using Garage_2._0.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Garage_2._0.Models.Entities;
+
 
 namespace Garage_2._0
 {
@@ -12,6 +15,8 @@ namespace Garage_2._0
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<Garage_2_0Context>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Garage_2_0Context") ?? throw new InvalidOperationException("Connection string 'Garage_2_0Context' not found.")));
+
+            builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Garage_2_0Context>();
 
             builder.Services.AddScoped<ISpotRepository, SpotRepository>();
             builder.Services.AddScoped<IFeedbackMessageRepository, FeedbackMessageRepository>();
@@ -35,6 +40,8 @@ namespace Garage_2._0
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.MapRazorPages();
 
             app.MapControllerRoute(
                 name: "default",
