45293fabf9b654ff0d5351fe1a44ae932e07ad8f
dansaa8 <dansaa8@protonmail.com>
2024-11-22 13:47:05 +0100

    Add VehicleType Seed

--- Garage 2.0/Models/Entities/SpotAllocation.cs
+++ Garage 2.0/Models/Entities/SpotAllocation.cs
@@ -1,14 +1,14 @@
-﻿namespace Garage_2._0.Models.Entities
-{
-    public class SpotAllocation
+﻿    namespace Garage_2._0.Models.Entities
     {
-        public int Id { get; set; }
-        public int SpotId { get; set; }
-        public Spot Spot { get; set; } // Navigation property
+        public class SpotAllocation
+        {
+            public int Id { get; set; }
+            public int SpotId { get; set; }
+            public Spot Spot { get; set; } // Navigation property
 
-        public int VehicleId { get; set; }
-        public Vehicle Vehicle { get; set; } // Navigation property
+            public int VehicleId { get; set; }
+            public Vehicle Vehicle { get; set; } // Navigation property
 
-        public decimal Fraction { get; set; } // E.g., 1.0 for full spot, 0.5 for half a spot
+            public decimal Fraction { get; set; } // E.g., 1.0 for full spot, 0.5 for half a spot
+        }
     }
-}
